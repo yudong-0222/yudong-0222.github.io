8d4bbfe8b87825e29bc228fd7d6d3b64e0af4906
RedFeaTherXll <yuchi9716@gmail.com>
2023-02-04 20:44:31 +0800

    Site updated: 2023-02-04 20:44:26

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
-window.addEventListener("DOMContentLoaded",()=>{REDEFINE.themeInfo={theme:"Redefine v"+REDEFINE.theme_config.version,author:"EvanNotFound",repository:"https://github.com/EvanNotFound/hexo-theme-redefine"},REDEFINE.localStorageKey="REDEFINE-THEME-STATUS",REDEFINE.styleStatus={isExpandPageWidth:!1,isDark:!1,fontSizeLevel:0,isOpenPageAside:!0},REDEFINE.printThemeInfo=()=>{console.log(`
- %c ${REDEFINE.themeInfo.theme} %c ${REDEFINE.themeInfo.repository} 
-`,"color: #fadfa3; background: #333; padding: 5px 0;","background: #fadfa3; padding: 5px 0;")},REDEFINE.setStyleStatus=()=>{localStorage.setItem(REDEFINE.localStorageKey,JSON.stringify(REDEFINE.styleStatus))},REDEFINE.getStyleStatus=()=>{var E=localStorage.getItem(REDEFINE.localStorageKey);if(E){for(var e in E=JSON.parse(E),REDEFINE.styleStatus)REDEFINE.styleStatus[e]=E[e];return E}return null},REDEFINE.refresh=()=>{REDEFINE.initUtils(),REDEFINE.initHeaderShrink(),REDEFINE.initModeToggle(),REDEFINE.initBack2Top(),!0===REDEFINE.theme_config.local_search.enable&&REDEFINE.initLocalSearch(),!0===REDEFINE.theme_config.code_block.copy&&REDEFINE.initCodeCopy(),!0===REDEFINE.theme_config.lazyload.enable&&REDEFINE.initLazyLoad()},REDEFINE.printThemeInfo(),REDEFINE.refresh()});
+window.addEventListener("DOMContentLoaded",()=>{REDEFINE.themeInfo={theme:"Redefine v"+REDEFINE.theme_config.version,author:"EvanNotFound",repository:"https://github.com/EvanNotFound/hexo-theme-redefine"},REDEFINE.localStorageKey="REDEFINE-THEME-STATUS",REDEFINE.styleStatus={isExpandPageWidth:!1,isDark:!1,fontSizeLevel:0,isOpenPageAside:!0},REDEFINE.printThemeInfo=()=>{console.log(`      ______ __  __  ______  __    __  ______                       \r
+     /\\__  _/\\ \\_\\ \\/\\  ___\\/\\ "-./  \\/\\  ___\\                      \r
+     \\/_/\\ \\\\ \\  __ \\ \\  __\\\\ \\ \\-./\\ \\ \\  __\\                      \r
+        \\ \\_\\\\ \\_\\ \\_\\ \\_____\\ \\_\\ \\ \\_\\ \\_____\\                    \r
+         \\/_/ \\/_/\\/_/\\/_____/\\/_/  \\/_/\\/_____/                    \r
+                                                               \r
+ ______  ______  _____   ______  ______ __  __   __  ______    \r
+/\\  == \\/\\  ___\\/\\  __-./\\  ___\\/\\  ___/\\ \\/\\ "-.\\ \\/\\  ___\\   \r
+\\ \\  __<\\ \\  __\\\\ \\ \\/\\ \\ \\  __\\\\ \\  __\\ \\ \\ \\ \\-.  \\ \\  __\\   \r
+ \\ \\_\\ \\_\\ \\_____\\ \\____-\\ \\_____\\ \\_\\  \\ \\_\\ \\_\\\\"\\_\\ \\_____\\ \r
+  \\/_/ /_/\\/_____/\\/____/ \\/_____/\\/_/   \\/_/\\/_/ \\/_/\\/_____/\r
+                                                               \r
+  Github: https://github.com/EvanNotFound/hexo-theme-redefine`)},REDEFINE.setStyleStatus=()=>{localStorage.setItem(REDEFINE.localStorageKey,JSON.stringify(REDEFINE.styleStatus))},REDEFINE.getStyleStatus=()=>{var _=localStorage.getItem(REDEFINE.localStorageKey);if(_){for(var E in _=JSON.parse(_),REDEFINE.styleStatus)REDEFINE.styleStatus[E]=_[E];return _}return null},REDEFINE.refresh=()=>{REDEFINE.initUtils(),REDEFINE.initMenuShrink(),REDEFINE.initModeToggle(),REDEFINE.initBackToTop(),!0===REDEFINE.theme_config.local_search.enable&&REDEFINE.initLocalSearch(),!0===REDEFINE.theme_config.code_block.copy&&REDEFINE.initCopyCode(),!0===REDEFINE.theme_config.lazyload.enable&&REDEFINE.initLazyLoad()},REDEFINE.printThemeInfo(),REDEFINE.refresh()});
